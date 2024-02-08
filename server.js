6a69c5914830b30cb3063dbc1037fd169d813e6e
vermaanurag1532 <104077846+vermaanurag1532@users.noreply.github.com>
2024-02-09 02:10:33 +0530

    css

--- server.js
+++ server.js
@@ -275,9 +275,10 @@ app.post('/process-document', upload.single('document'), (req, res) => {
 });
 
 
-
+const PORT = process.env.PORT || 3000;
 
 // Start server
-app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+
 });
